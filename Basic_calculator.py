89c0272fe1276fa40620675be043567a12edbb0d
saikumar kothada <saikumarkothada@gmail.com>
2024-12-08 17:51:31 +0000

    added test program files

---
+++ Basic_calculator.py
@@ -0,0 +1,9 @@
+'''hello all this is my code'''
+
+def addition(num1, num2):
+    sum = num1 + num2
+    return sum
+ 
+def sub(num1, num2):
+    sum = num1 - num2
+    return sum
---
+++ adwance_calc.py
@@ -0,0 +1,5 @@
+import Basic_calculator as calc
+
+print(calc.addition(45, 56))
+
+print(calc.sub(34, 4))
